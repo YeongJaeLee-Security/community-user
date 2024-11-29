b78d4bd80d781d51164efbd3a8303aaab0ff89ac
YeongJaeLee-Security <dudwo8528@gmail.com>
2024-11-29 15:01:10 +0900

    이미지 가운데 정렬

--- src/components/feed.js
+++ src/components/feed.js
@@ -10,7 +10,7 @@ export default function Feed({ posts }) {
       {posts.map((post) => (
         <Card
           key={post.id}
-          sx={{ mb: 2, cursor: 'pointer' }}
+          sx={{ mb: 2, cursor: 'pointer', borderRadius: 2 }}
           onClick={() => router.push(`/post/${post.id}`)}
         >
           <CardContent>
@@ -19,7 +19,17 @@ export default function Feed({ posts }) {
               {post.content}
             </Typography>
             {post.image_path && (
-              <Box component="img" sx={{width: '600px', height: '500px', objectFit: 'cover'}} src={`http://localhost:8000/${post.image_path}`}></Box>
+              <Box
+                component="img"
+                sx={{
+                  display: 'block', // 이미지 블록 처리
+                  margin: '0 auto', // 수평 가운데 정렬
+                  width: '600px',
+                  height: '500px',
+                  objectFit: 'cover',
+                }}
+                src={`http://localhost:8000/${post.image_path}`}
+              />
             )}
           </CardContent>
         </Card>
